eb656c16df5cda2840537fa86f32c51b101f89ec
Brittney Brinson <brittneyc95@outlook.com>
2023-01-14 23:59:31 -0800

    Function practice

---
+++ practice_functions.js
@@ -0,0 +1,8 @@
+//sum of numbers within an array
+let arrayOfNumbers = [2,4,6,8];
+let sum = 0 ;
+function addElements (thing){
+    sum =+ thing
+}
+// console.log(arrayOfNumbers.forEach(addElements));
+
